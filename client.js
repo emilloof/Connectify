b83f227ff019b39111973627b2d74c74ddf87840
emilo851 <emilo851@student.liu.se>
2025-01-23 19:40:33 +0100

    put our work togheter with minor fixes (Step 7)

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ function displayProfile(){
     let token = localStorage.getItem('token');
     if(token != null){
         document.body.innerHTML = document.getElementById("profilescript").innerHTML;
+        document.getElementById("default").click();
     }
 
     else{
@@ -69,3 +70,50 @@ function isLongPassword(password) {
 }
 
 window.onload = displayProfile;
+
+
+function openTab(pageName, element){
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(pageName).style.display = "block";
+    element.style.backgroundColor = "red";
+}
+
+function changePassword(input){
+    try{
+        let token = localStorage.getItem('token');
+        let oldPassword = input.oldpassword.value;
+        let newPassword = input.newpassword1.value;
+        let newPassword2 = input.newpassword2.value;
+        if(newPassword != newPassword2){
+            document.getElementById('feedbackChange').innerHTML = "The two new passwords are not the same!";
+        }
+        else if(!isLongPassword(newPassword)){
+            document.getElementById('feedbackChange').innerHTML = "Minimum password length is 8 chars!";
+             }
+        else{
+            let answer = serverstub.changePassword(token, oldPassword, newPassword)
+            document.getElementById('feedbackChange').innerHTML = answer.message;
+        }
+    }
+    catch(e){
+        document.getElementById('feedbackChange').innerHTML = e.message;
+    }
+}
+
+function signOut(){
+    let token = localStorage.getItem('token');
+    let answer = serverstub.signOut(token);
+    localStorage.removeItem('token'); //maybe use "" instead of null
+    displayProfile();
+    document.getElementById('feedbackLogin').innerHTML = answer.message;
+}
