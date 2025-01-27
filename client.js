eaf7741b52f55df52b7db346e8e62dc76cc299c5
emilo851 <emilo851@student.liu.se>
2025-01-27 11:32:57 +0100

    error code

--- client.js
+++ client.js
@@ -3,8 +3,8 @@ function displayProfile(){
     if(token != null){
         document.body.innerHTML = document.getElementById("profilescript").innerHTML;
         document.getElementById("default").click();
-        displayUserInfo();
-        reloadWall();
+        displayHomeInfo();
+        homeReloadWall();
     }
 
     else{
@@ -116,11 +116,12 @@ function signOut(){
     let token = localStorage.getItem('token');
     let answer = serverstub.signOut(token);
     localStorage.removeItem('token'); //maybe use "" instead of null
+    localStorage.removeItem('email');
     displayProfile();
     document.getElementById('feedbackLogin').innerHTML = answer.message;
 }
 
-function displayUserInfo(){
+function displayHomeInfo(){
     let token = localStorage.getItem('token');
     let user = serverstub.getUserDataByToken(token).data;
     let data = `
@@ -135,8 +136,24 @@ function displayUserInfo(){
         document.getElementById("homeuserinfo").innerHTML = data;
 }
 
-function sendMessage(data){
-    document.getElementById('feedbackMessage').innerHTML = "";
+function displayBrowseInfo(){
+    let token = localStorage.getItem('token');
+    let email = localStorage.getItem('email');
+    let user = serverstub.getUserDataByEmail(token, email).data;
+    let data = `
+        <h2>Profile</h2>
+        <p>First Name: ${user.firstname}</p>
+        <p>Family Name: ${user.familyname}</p>
+        <p>Gender: ${user.gender}</p>
+        <p>City: ${user.city}</p>
+        <p>Country: ${user.country}</p>
+        <p>Email: ${user.email}</p>
+        `
+        document.getElementById("browseuserinfo").innerHTML = data;
+}
+
+function homeSendMessage(data){
+    document.getElementById('homefeedbackmessage').innerHTML = "";
     let token = localStorage.getItem('token');
     let user = serverstub.getUserDataByToken(token).data;
     let email = user.email;
@@ -144,32 +161,77 @@ function sendMessage(data){
     if(message.length != 0){
         let answer = serverstub.postMessage(token, message, email);
         if (answer.success){
-            document.getElementById("messagefield").value = "";
-            reloadWall();
+            document.getElementById("homemessagefield").value = "";
+            homeReloadWall();
         }
         else{
-            document.getElementById('feedbackMessage').innerHTML = answer.message
+            document.getElementById('homefeedbackmessage').innerHTML = answer.message
         }
     }
     else{
-        document.getElementById('feedbackMessage').innerHTML = "Message field is empty!";
+        document.getElementById('homefeedbackmessage').innerHTML = "Message field is empty!";
     }
-    
 }
 
-function reloadWall(){
-    document.getElementById("messagewall").innerHTML = "";
+function browseSendMessage(data){
+    document.getElementById('browsefeedbackmessage').innerHTML = "";
+    let token = localStorage.getItem('token');
+    let email = localStorage.getItem('email');
+    let message = data.messagefield.value;
+    if(message.length != 0){
+        let answer = serverstub.postMessage(token, message, email);
+        if (answer.success){
+            document.getElementById("browsemessagefield").value = "";
+            reloadWall();
+        }
+        else{
+            document.getElementById('browsefeedbackmessage').innerHTML = answer.message
+        }
+    }
+    else{
+        document.getElementById('browsefeedbackmessage').innerHTML = "Message field is empty!";
+    }
+}
+
+function homeReloadWall(){
+    document.getElementById("homemessagewall").innerHTML = "";
     let token = localStorage.getItem('token');
     let messagewall = (serverstub.getUserMessagesByToken(token)).data;
   
     for (let index in messagewall) {
         let output = '<div>' + messagewall[index].writer + ': ' + messagewall[index].content + '</div>';
-        document.getElementById("messagewall").innerHTML += output;
+        document.getElementById("homemessagewall").innerHTML += output;
     }
 }
 
-function searchUser(email){
+function browseReloadWall(){
+    document.getElementById("browsemessagewall").innerHTML = "";
     let token = localStorage.getItem('token');
-    document.getElementById('home').style.display = "block";
-    reloadWall(email);
+    let email = localStorage.getItem('email');
+    let messagewall = (serverstub.getUserMessagesByEmail(token, email)).data;
+  
+    for (let index in messagewall) {
+        let output = '<div>' + messagewall[index].writer + ': ' + messagewall[index].content + '</div>';
+        document.getElementById("browsemessagewall").innerHTML += output;
+    }
+}
+
+function searchUser(input){
+    let token = localStorage.getItem('token');
+    let searchEmail = input.searchemail.value;
+    let user = serverstub.getUserDataByToken(token).data;
+    let userEmail = user.email;
+    document.getElementById("feedbacksearch").innerHTML = "";
+    if (userEmail === searchEmail){
+        document.getElementById("feedbacksearch").innerHTML = "You cannot search your own email!";
+    }
+    else if(serverstub.getUserMessagesByEmail(token, searchEmail).success){
+        document.getElementById('browsemessagediv').style.display = "block";
+        localStorage.setItem('email', searchEmail);
+        displayBrowseInfo();
+        browseReloadWall();
+    }
+    else {
+        document.getElementById("feedbacksearch").innerHTML = "Email does not exist!";
+    }
 }
